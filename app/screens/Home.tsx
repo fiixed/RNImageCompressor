c6ac0a9e5874d483ea2f60144a0b69acc3cd7e70
Andrew Bell <fiixed@gmail.com>
2023-09-21 11:53:37 -0500

    Title text and styling

--- app/screens/Home.tsx
+++ app/screens/Home.tsx
@@ -6,6 +6,13 @@ interface Props {}
 const Home: FC<Props> = (): JSX.Element => {
   return (
     <View style={styles.container}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Choose Your Image</Text>
+        <Text style={styles.secondaryText}>
+          You can select your image using one of these option which you want to
+          convert to passport size.
+        </Text>
+      </View>
       {/* Image Capture button */}
       <View style={styles.btnContainer}>
         <TouchableOpacity style={styles.button}>
@@ -30,6 +37,26 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  titleContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    paddingTop: 10,
+  },
+  title: {
+    fontSize: 25,
+    color: '#272727',
+    fontWeight: '500',
+    textAlign: 'center',
+  },
+  secondaryText: {
+    color: '#272727',
+    textAlign: 'center',
+    opacity: 0.5,
+    lineHeight: 20,
+    paddingTop: 5,
   },
   btnContainer: {
     width: 120,

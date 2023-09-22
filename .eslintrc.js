6e720c2a281975a65878044e90fbdde25ba99e36
Andrew Bell <fiixed@gmail.com>
2023-09-22 12:07:23 -0500

    LargeIconButton

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,6 @@ module.exports = {
   root: true,
   extends: '@react-native',
   rules: {
-    // ...
     'react/react-in-jsx-scope': 'off',
     'react/jsx-uses-react': 'off',
   },
--- app/screens/Home.tsx
+++ app/screens/Home.tsx
@@ -1,9 +1,15 @@
 import {FC} from 'react';
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {StyleSheet, Text, View, PermissionsAndroid} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome5';
+import LargeIconButton from '../components/LargeIconButton';
 
 interface Props {}
 const Home: FC<Props> = (): JSX.Element => {
+  const handleImageCapture = async (): Promise<void> => {
+    console.log('====================================');
+    console.log('test');
+    console.log('====================================');
+  };
   return (
     <View style={styles.container}>
       <View style={styles.titleContainer}>
@@ -14,20 +20,13 @@ const Home: FC<Props> = (): JSX.Element => {
         </Text>
       </View>
       {/* Image Capture button */}
-      <View style={styles.btnContainer}>
-        <TouchableOpacity style={styles.button}>
-          <Icon style={styles.icon} name="camera" />
-        </TouchableOpacity>
-        <Text style={styles.btnLabel}>Capture</Text>
-      </View>
-
+      <LargeIconButton onPress={handleImageCapture} title="Capture">
+        <Icon name="camera" />
+      </LargeIconButton>
       {/* Image Select Button */}
-      <View style={styles.btnContainer}>
-        <TouchableOpacity style={styles.button}>
-          <Icon style={styles.icon} name="folder-open" />
-        </TouchableOpacity>
-        <Text style={styles.btnLabel}>Select</Text>
-      </View>
+      <LargeIconButton title="Select">
+        <Icon name="folder-open" />
+      </LargeIconButton>
     </View>
   );
 };
@@ -57,28 +56,6 @@ const styles = StyleSheet.create({
     opacity: 0.5,
     lineHeight: 20,
     paddingTop: 5,
-  },
-  btnContainer: {
-    width: 120,
-    height: 120,
-    marginVertical: 25,
-  },
-  button: {
-    width: '100%',
-    height: '100%',
-    borderWidth: 4,
-    borderColor: '#6C9ADE',
-    borderRadius: 7,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  btnLabel: {
-    textAlign: 'center',
-    fontWeight: '500',
-  },
-  icon: {
-    color: '#6C9ADE',
-    fontSize: 55,
   },
 });
 
